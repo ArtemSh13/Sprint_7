c407f92675065de8c2677aaf43b32c38e43461f8
ArtemSh13 <mozg1303@ya.ru>
2022-09-05 01:23:14 +0300

    Add parameterization to test of order creation

--- src/test/java/OrdersCreateTest.java
+++ src/test/java/OrdersCreateTest.java
@@ -1,19 +1,54 @@
+import com.google.gson.Gson;
 import io.qameta.allure.junit4.DisplayName;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.Matchers.equalTo;
 
-import java.util.Random;
 
+@RunWith(Parameterized.class)
 public class OrdersCreateTest {
 
-    int track;
+    private final String firstName;
+    private final String lastName;
+    private final String address;
+    private final String metroStation;
+    private final String phone;
+    private final int rentTime;
+    private final String deliveryDate;
+    private final String comment;
+    private final String[] color;
+    private int track;
+
+    public OrdersCreateTest(String[] color) {
+        this.firstName = DataGenerator.getRandomFirstName();
+        this.lastName = DataGenerator.getRandomLastName();
+        this.address = DataGenerator.getRandomAddress();
+        this.metroStation = DataGenerator.getRandomMetroStation();
+        this.phone = DataGenerator.getRandomPhone();
+        this.rentTime = DataGenerator.getRandomRentTime();
+        this.deliveryDate = DataGenerator.getRandomDeliveryDate();
+        this.comment = DataGenerator.getRandomComment();
+        this.color = color;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getRequestData() {
+        return new Object[][]{
+                {new String[]{"BLACK"}},
+                {new String[]{"GREY"}},
+                {new String[]{"BLACK", "GREY"}},
+                {new String[]{}}
+
+        };
+    }
 
     @Before
     public void setUp() {
@@ -22,114 +57,14 @@ public class OrdersCreateTest {
 
     // можно указать один из цветов — BLACK или GREY
     @Test
-    @DisplayName("createOrdersWithBLACKAndCheckResponse")
-    public void createOrdersWithBLACKAndCheckResponse() {
-        Random random = new Random();
-        String firstName = "firstName" + random.nextInt(999999);
-        String lastName = "lastName" + random.nextInt(999999);
-        String address = "address" + random.nextInt(999999);
-        String metroStation = "metroStation" + random.nextInt(999999);
-        String phone = "phone" + random.nextInt(999999);
-        int rentTime = random.nextInt(999);
-        String deliveryDate = "2020-06-06";
-        String comment = "comment" + random.nextInt(999999);
-        String[] color = {"BLACK"};
+    @DisplayName("createOrdersAndCheckResponse")
+    public void createOrdersAndCheckResponse() {
 
         Orders orders = new Orders(firstName, lastName, address, metroStation, phone, rentTime, deliveryDate, comment, color);
 
-        Response response =
-                given()
-                        .header("Content-type", "application/json")
-                        .and()
-                        .body(orders)
-                        .when()
-                        .post(ScooterAPI.CREATE_ORDERS_API);
-
-        response.then().assertThat().body("track", notNullValue())
-                .and().statusCode(201);
-
-        track = response.then().extract().path("track");
-
-    }
-
-    @Test
-    @DisplayName("createOrdersWithGREYAndCheckResponse")
-    public void createOrdersWithGREYAndCheckResponse() {
-        Random random = new Random();
-        String firstName = "firstName" + random.nextInt(999999);
-        String lastName = "lastName" + random.nextInt(999999);
-        String address = "address" + random.nextInt(999999);
-        String metroStation = "metroStation" + random.nextInt(999999);
-        String phone = "phone" + random.nextInt(999999);
-        int rentTime = random.nextInt(999);
-        String deliveryDate = "2020-06-06";
-        String comment = "comment" + random.nextInt(999999);
-        String[] color = {"GREY"};
-
-        Orders orders = new Orders(firstName, lastName, address, metroStation, phone, rentTime, deliveryDate, comment, color);
-
-        Response response =
-                given()
-                        .header("Content-type", "application/json")
-                        .and()
-                        .body(orders)
-                        .when()
-                        .post(ScooterAPI.CREATE_ORDERS_API);
-        System.out.println(response.asPrettyString());
-        response.then().assertThat().body("track", notNullValue())
-                .and().statusCode(201);
-
-        track = response.then().extract().path("track");
-
-    }
-
-    // можно указать оба цвета
-    @Test
-    @DisplayName("createOrdersWithBLACKAndGREYAndCheckResponse")
-    public void createOrdersWithBLACKAndGREYAndCheckResponse() {
-        Random random = new Random();
-        String firstName = "firstName" + random.nextInt(999999);
-        String lastName = "lastName" + random.nextInt(999999);
-        String address = "address" + random.nextInt(999999);
-        String metroStation = "metroStation" + random.nextInt(999999);
-        String phone = "phone" + random.nextInt(999999);
-        int rentTime = random.nextInt(999);
-        String deliveryDate = "2020-06-06";
-        String comment = "comment" + random.nextInt(999999);
-        String[] color = {"BLACK", "GREY"};
-
-        Orders orders = new Orders(firstName, lastName, address, metroStation, phone, rentTime, deliveryDate, comment, color);
-
-        Response response =
-                given()
-                        .header("Content-type", "application/json")
-                        .and()
-                        .body(orders)
-                        .when()
-                        .post(ScooterAPI.CREATE_ORDERS_API);
-
-        response.then().assertThat().body("track", notNullValue())
-                .and().statusCode(201);
-
-        track = response.then().extract().path("track");
-
-    }
-    // можно совсем не указывать цвет
-    @Test
-    @DisplayName("createOrdersWithoutColorAndCheckResponse")
-    public void createOrdersWithoutColorAndCheckResponse() {
-        Random random = new Random();
-        String firstName = "firstName" + random.nextInt(999999);
-        String lastName = "lastName" + random.nextInt(999999);
-        String address = "address" + random.nextInt(999999);
-        String metroStation = "metroStation" + random.nextInt(999999);
-        String phone = "phone" + random.nextInt(999999);
-        int rentTime = random.nextInt(999);
-        String deliveryDate = "2020-06-06";
-        String comment = "comment" + random.nextInt(999999);
-        String[] color = {};
-
-        Orders orders = new Orders(firstName, lastName, address, metroStation, phone, rentTime, deliveryDate, comment, color);
+        Gson gson = new Gson();
+        String json = gson.toJson(orders);
+        System.out.println(json);
 
         Response response =
                 given()
